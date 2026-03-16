3b7e56e8e95b236d3e89411749a70f82780a35c4
Igor Santos <igorsantos69142@gmail.com>
2026-03-16 00:16:37 -0400

    ⚙️ config: ajuste em arquivos de configuracao

--- config/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv())
-
-# databricks_df
-DBX_DF_HOST_DF_API_KEY = os.getenv("DBX_DF_HOST_DF_API_KEY")
-DBX_DF_HOST = os.getenv("DBX_DF_HOST")
